f601785faddaab8b29e80050ac42dcc99c760849
Olly Filmer <ollyfilmer@yahoo.co.uk>
2023-07-25 18:14:22 +0100

    removed unused middleware and cleaned up celeritas reference

--- routes.go
+++ routes.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"myapp/data"
 	"net/http"
-	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
 
 // This basically Adds routes to the existing routes.
 func (a *application) routes() *chi.Mux {
-	// Middleware must come before any routes
-	a.use(a.Middleware.CheckRemember)
-
 	// Add routes here
 	a.get("/", a.Handlers.Home)
 
